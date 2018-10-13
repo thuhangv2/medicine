c0c890601e2d0c6b6fccc894a1febe3cf3345217
Naruto <lanhktc@gmail.com>
2018-10-13 21:29:54 +0700

    fix error install composer

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -27,11 +27,38 @@ class GeneralController extends Controller
 
     public function __construct()
     {
+
+        //=======Config====
+        //Config for  SMTP
+        $configs        = Config::pluck('value', 'key')->all();
+        $configs_global = ConfigGlobal::first();
+        config(['app.name' => $configs_global['title']]);
+        config(['mail.driver' => ($configs['smtp_mode']) ? 'smtp' : 'sendmail']);
+        config(['mail.host' => empty($configs['smtp_host']) ? env('MAIL_HOST', '') : $configs['smtp_host']]);
+        config(['mail.port' => empty($configs['smtp_port']) ? env('MAIL_PORT', '') : $configs['smtp_port']]);
+        config(['mail.encryption' => empty($configs['smtp_security']) ? env('MAIL_ENCRYPTION', '') : $configs['smtp_security']]);
+        config(['mail.username' => empty($configs['smtp_user']) ? env('MAIL_USERNAME', '') : $configs['smtp_user']]);
+        config(['mail.password' => empty($configs['smtp_password']) ? env('MAIL_PASSWORD', '') : $configs['smtp_password']]);
+        config(['mail.from' =>
+            ['address' => $configs_global['email'], 'name' => $configs_global['title']]]
+        );
+        //SMTP
+
+        config(['admin.operation_log.enable' => empty($configs['admin_log']) ? false : $configs['admin_log']]);
+
+        //============end config====
+
+        //Auto switch link to https
+        if (!empty($configs['site_ssl'])) {
+            \URL::forceScheme('https');
+        }
+        //end
+
         $host = request()->getHost();
         config(['app.url' => 'http://' . $host]);
         $this->path_file      = config('filesystems.disks.path_file', '');
-        $this->configs_global = ConfigGlobal::first();
-        $this->configs        = Config::pluck('value', 'key')->all();
+        $this->configs_global = $configs_global;
+        $this->configs        = $configs;
         $this->theme_asset    = 'scart_templates/' . $this->configs_global['template'];
         $this->theme          = 'scart_templates.' . $this->configs_global['template'];
         $this->banners        = Banner::where('status', 1)->orderBy('sort', 'desc')->orderBy('id', 'desc')->get();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,31 +14,6 @@ class AppServiceProvider extends ServiceProvider
     // public function boot()
     public function boot()
     {
-        //=======Config====
-        //Config for  SMTP
-        $configs        = \App\Models\Config::pluck('value', 'key')->all();
-        $configs_global = \App\Models\ConfigGlobal::first();
-        config(['app.name' => $configs_global['title']]);
-        config(['mail.driver' => ($configs['smtp_mode']) ? 'smtp' : 'sendmail']);
-        config(['mail.host' => empty($configs['smtp_host']) ? env('MAIL_HOST', '') : $configs['smtp_host']]);
-        config(['mail.port' => empty($configs['smtp_port']) ? env('MAIL_PORT', '') : $configs['smtp_port']]);
-        config(['mail.encryption' => empty($configs['smtp_security']) ? env('MAIL_ENCRYPTION', '') : $configs['smtp_security']]);
-        config(['mail.username' => empty($configs['smtp_user']) ? env('MAIL_USERNAME', '') : $configs['smtp_user']]);
-        config(['mail.password' => empty($configs['smtp_password']) ? env('MAIL_PASSWORD', '') : $configs['smtp_password']]);
-        config(['mail.from' =>
-            ['address' => $configs_global['email'], 'name' => $configs_global['title']]]
-        );
-        //SMTP
-        //
-        config(['admin.operation_log.enable' => empty($configs['admin_log']) ? false : $configs['admin_log']]);
-
-        //============end config====
-
-        //Auto switch link to https
-        if (!empty($configs['site_ssl'])) {
-            \URL::forceScheme('https');
-        }
-        //end
 
     }
 
