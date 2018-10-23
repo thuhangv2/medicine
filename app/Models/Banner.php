47bc5727d30eed80f0531c72acc563d0d6dff3fa
Naruto <lanhktc@gmail.com>
2018-10-23 22:00:25 +0700

    no message

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     public $table = 'banner';
+    /**
+     * [getImage description]
+     * @return [type] [description]
+     */
+    public function getImage()
+    {
+        $path_file = config('filesystems.disks.path_file', '');
+        return $path_file . '/' . $this->image;
+
+    }
 }
