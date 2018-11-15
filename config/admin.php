082c76cb234d671b03b89d1cc75dcd548606b67f
Naruto <lanhktc@gmail.com>
2018-11-15 10:51:46 +0700

    no message

--- config/admin.php
+++ config/admin.php
@@ -3,24 +3,50 @@
 return [
 
     /*
-     * Laravel-admin name.
+    |--------------------------------------------------------------------------
+    | Laravel-admin name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of laravel-admin, This setting is displayed on the
+    | login page.
+    |
      */
-    'name'          => 'S-cart Admin',
+    'name'                      => 'S-cart Admin',
 
     /*
-     * Logo in admin panel header.
+    |--------------------------------------------------------------------------
+    | Laravel-admin logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages. You can also set it as an image by using a
+    | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
+    |
      */
-    'logo'          => '<img src="/images/scart-min.png">',
+    'logo'                      => '<img src="/images/scart-min.png">',
 
     /*
-     * Mini-logo in admin panel header.
+    |--------------------------------------------------------------------------
+    | Laravel-admin mini logo
+    |--------------------------------------------------------------------------
+    |
+    | The logo of all admin pages when the sidebar menu is collapsed. You can
+    | also set it as an image by using a `img` tag, eg
+    | '<img src="http://logo-url" alt="Admin logo">'.
+    |
      */
-    'logo-mini'     => '<b>S-cart</b>',
+    'logo-mini'                 => '<b>S-cart</b>',
 
     /*
-     * Route configuration.
+    |--------------------------------------------------------------------------
+    | Laravel-admin route settings
+    |--------------------------------------------------------------------------
+    |
+    | The routing configuration of the admin page, including the path prefix,
+    | the controller namespace, and the default middleware. If you want to
+    | access through the root path, just set the prefix to empty string.
+    |
      */
-    'route'         => [
+    'route'                     => [
 
         'prefix'     => 'system_admin',
 
@@ -30,32 +56,60 @@ return [
     ],
 
     /*
-     * Laravel-admin install directory.
+    |--------------------------------------------------------------------------
+    | Laravel-admin install directory
+    |--------------------------------------------------------------------------
+    |
+    | The installation directory of the controller and routing configuration
+    | files of the administration page. The default is `app/Admin`, which must
+    | be set before running `artisan admin::install` to take effect.
+    |
      */
-    'directory'     => app_path('Admin'),
+    'directory'                 => app_path('Admin'),
 
     /*
-     * Laravel-admin html title.
+    |--------------------------------------------------------------------------
+    | Laravel-admin html title
+    |--------------------------------------------------------------------------
+    |
+    | Html title for all pages.
+    |
      */
-    'title'         => 'S-cart Admin',
+    'title'                     => 'Admin',
 
     /*
-     * Use `https`.
+    |--------------------------------------------------------------------------
+    | Access via `https`
+    |--------------------------------------------------------------------------
+    |
+    | If your page is going to be accessed via https, set it to `true`.
+    |
      */
-    'secure'        => false,
+    'https'                     => env('ADMIN_HTTPS', false),
 
     /*
-     * Laravel-admin auth setting.
+    |--------------------------------------------------------------------------
+    | Laravel-admin auth setting
+    |--------------------------------------------------------------------------
+    |
+    | Authentication settings for all admin pages. Include an authentication
+    | guard and a user provider setting of authentication driver.
+    |
+    | You can specify a controller for `login` `logout` and other auth routes.
+    |
      */
-    'auth'          => [
-        'guards'    => [
+    'auth'                      => [
+
+        'controller' => App\Admin\Controllers\AuthController::class,
+
+        'guards'     => [
             'admin' => [
                 'driver'   => 'session',
                 'provider' => 'admin',
             ],
         ],
 
-        'providers' => [
+        'providers'  => [
             'admin' => [
                 'driver' => 'eloquent',
                 'model'  => Encore\Admin\Auth\Database\Administrator::class,
@@ -64,12 +118,20 @@ return [
     ],
 
     /*
-     * Laravel-admin upload setting.
+    |--------------------------------------------------------------------------
+    | Laravel-admin upload setting
+    |--------------------------------------------------------------------------
+    |
+    | File system configuration for form upload files and images, including
+    | disk and upload path.
+    |
      */
-    'upload'        => [
+    'upload'                    => [
 
+        // Disk in `config/filesystem.php`.
         'disk'      => 'admin',
 
+        // Image and file upload path under the disk above.
         'directory' => [
             'image' => 'images',
             'file'  => 'files',
@@ -77,9 +139,14 @@ return [
     ],
 
     /*
-     * Laravel-admin database setting.
+    |--------------------------------------------------------------------------
+    | Laravel-admin database settings
+    |--------------------------------------------------------------------------
+    |
+    | Here are database settings for laravel-admin builtin model & tables.
+    |
      */
-    'database'      => [
+    'database'                  => [
 
         // Database connection for following tables.
         'connection'             => '',
@@ -109,48 +176,142 @@ return [
     ],
 
     /*
-     * By setting this option to open or close operation log in laravel-admin.
+    |--------------------------------------------------------------------------
+    | User operation log setting
+    |--------------------------------------------------------------------------
+    |
+    | By setting this option to open or close operation log in laravel-admin.
+    |
      */
-    'operation_log' => [
+    'operation_log'             => [
 
-        'enable' => false,
+        'enable'          => true,
+
+        /*
+         * Only logging allowed methods in the list
+         */
+        'allowed_methods' => ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'],
 
         /*
          * Routes that will not log to database.
          *
          * All method to path like: admin/auth/logs
-         * or specific method to path like: get:admin/auth/logs
+         * or specific method to path like: get:admin/auth/logs.
          */
-        'except' => [
+        'except'          => [
             'admin/auth/logs*',
         ],
     ],
 
     /*
-     * @see https://adminlte.io/docs/2.4/layout
+    |--------------------------------------------------------------------------
+    | Admin map field provider
+    |--------------------------------------------------------------------------
+    |
+    | Supported: "tencent", "google", "yandex".
+    |
      */
-    'skin'          => 'skin-blue',
+    'map_provider'              => 'google',
 
     /*
-    |---------------------------------------------------------|
-    |LAYOUT OPTIONS | fixed                                   |
-    |               | layout-boxed                            |
-    |               | layout-top-nav                          |
-    |               | sidebar-collapse                        |
-    |               | sidebar-mini                            |
-    |---------------------------------------------------------|
+    |--------------------------------------------------------------------------
+    | Application Skin
+    |--------------------------------------------------------------------------
+    |
+    | This value is the skin of admin pages.
+    | @see https://adminlte.io/docs/2.4/layout
+    |
+    | Supported:
+    |    "skin-blue", "skin-blue-light", "skin-yellow", "skin-yellow-light",
+    |    "skin-green", "skin-green-light", "skin-purple", "skin-purple-light",
+    |    "skin-red", "skin-red-light", "skin-black", "skin-black-light".
+    |
      */
-    'layout'        => ['sidebar-mini'],
+    'skin'                      => 'skin-blue',
 
     /*
-     * Version displayed in footer.
+    |--------------------------------------------------------------------------
+    | Application layout
+    |--------------------------------------------------------------------------
+    |
+    | This value is the layout of admin pages.
+    | @see https://adminlte.io/docs/2.4/layout
+    |
+    | Supported: "fixed", "layout-boxed", "layout-top-nav", "sidebar-collapse",
+    | "sidebar-mini".
+    |
      */
-    'version'       => '1.5.x-dev',
+    'layout'                    => ['sidebar-mini'],
 
     /*
-     * Settings for extensions.
+    |--------------------------------------------------------------------------
+    | Login page background image
+    |--------------------------------------------------------------------------
+    |
+    | This value is used to set the background image of login page.
+    |
      */
-    'extensions'    => [
+    'login_background_image'    => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Show version at footer
+    |--------------------------------------------------------------------------
+    |
+    | Whether to display the version number of laravel-admim at the footer of
+    | each page
+    |
+     */
+    'show_version'              => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Show environment at footer
+    |--------------------------------------------------------------------------
+    |
+    | Whether to display the environment at the footer of each page
+    |
+     */
+    'show_environment'          => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu bind to permission
+    |--------------------------------------------------------------------------
+    |
+    | whether enable menu bind to a permission
+     */
+    'menu_bind_permission'      => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable default breadcrumb
+    |--------------------------------------------------------------------------
+    |
+    | Whether enable default breadcrumb for every page content.
+     */
+    'enable_default_breadcrumb' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Extension Directory
+    |--------------------------------------------------------------------------
+    |
+    | When you use command `php artisan admin:extend` to generate extensions,
+    | the extension files will be generated in this directory.
+     */
+    'extension_dir'             => app_path('Admin/Extensions'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings for extensions.
+    |--------------------------------------------------------------------------
+    |
+    | You can find all available extensions here
+    | https://github.com/laravel-admin-extensions.
+    |
+     */
+    'extensions'                => [
 
     ],
 ];
