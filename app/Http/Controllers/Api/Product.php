d7b3f09f6a44f469409aae1a24a30f14ea922402
Naruto <lanhktc@gmail.com>
2018-12-25 15:35:02 +0700

    no message

--- app/Http/Controllers/Api/Product.php
+++ app/Http/Controllers/Api/Product.php
@@ -2,114 +2,27 @@
 #app/Http/Controller/Api/Product.php
 namespace App\Http\Controllers\Api;
 
-use App\Models\ShopApi;
-use App\Models\ShopApiProcess;
 use App\Models\ShopProduct;
 use Illuminate\Http\Request;
 
-class Product extends \App\Http\Controllers\Controller
+class Product extends General
 {
-    public $hiddenFields;
+
     public $limit;
     public $start;
     public $orderBy;
     public $sort;
-    public $apiName;
-    public $secretKey;
     public $model;
 
     public function __construct(Request $request)
     {
-        $ipClient        = $request->ip();
-        $method          = $request->method();
-        $headers         = getallheaders();
-        $this->secretKey = $headers['Apisecret'] ?? '';
-        $this->apiName   = $headers['Apiname'] ?? '';
-        $data            = $request->all();
-        $this->limit     = empty($data['limit']) ? 10 : $data['limit'];
-        $this->start     = empty($data['start']) ? 0 : (int) $data['start'];
-        $this->orderBy   = empty($data['orderBy']) ? [] : explode(',', $data['orderBy']);
-        $this->sort      = $data['sort'] ?? 'ASC';
-        $this->model     = empty($data['model']) ? '' : $data['model'];
-
-        $error = array();
-
-        //Check API
-        $checkApi = ShopApi::where('name', $this->apiName)->first();
-        if (!$checkApi || $checkApi->type === 'private') {
-//==============Process when Api is private or not found ============
-            $error = array(
-                'error'  => 1,
-                'code'   => 404,
-                'detail' => 'Api name not found or private',
-                'msg'    => 'Not found',
-            );
-            header('Content-Type: application/json');
-            echo json_encode($error, JSON_UNESCAPED_UNICODE);
-            exit;
-        }
-        if ($checkApi->type === 'secret') {
-//==============Process when Api is secret============
-            //Check Token
-            $apiId          = $checkApi->id;
-            $checkSecretKey = ShopApiProcess::where('secret_key', $this->secretKey)->where('api_id', $apiId)->first();
-            if (!$checkSecretKey) {
-                $error = array(
-                    'error'  => 1,
-                    'code'   => 404,
-                    'detail' => 'Secret key not found',
-                    'msg'    => 'Not found',
-                );
-                header('Content-Type: application/json');
-                echo json_encode($error, JSON_UNESCAPED_UNICODE);
-                exit;
-            }
-
-            if (!$checkSecretKey->status) {
-                $error = array(
-                    'error'  => 1,
-                    'code'   => 403,
-                    'detail' => 'Secret key invalid',
-                    'msg'    => 'Access denied',
-                );
-            } else {
-                $exp     = $checkSecretKey->exp;
-                $ipAllow = $checkSecretKey->ip_allow;
-                $ipDeny  = $checkSecretKey->ip_deny;
-
-                //Check Ip
-                if (($ipAllow && !in_array($ipClient, explode(',', $ipAllow))) ||
-                    (!$ipAllow && in_array($ipClient, explode(',', $ipDeny)))
-                ) {
-                    $error = array(
-                        'error'  => 1,
-                        'code'   => 403,
-                        'detail' => 'Your IP ' . $ipClient . ' denied!',
-                        'msg'    => 'Access denied',
-                    );
-                }
-                //Check Expire
-                elseif ($exp && $exp <= date('Y-m-d H:i:s')) {
-                    $error = array(
-                        'error'  => 1,
-                        'code'   => 403,
-                        'detail' => 'Secret key ' . $this->secretKey . ' expire!',
-                        'msg'    => 'Access denied',
-                    );
-                }
-            }
-
-            if ($error) {
-                header('Content-Type: application/json');
-                echo json_encode($error, JSON_UNESCAPED_UNICODE);
-                exit;
-            }
-            $this->hiddenFields = $checkSecretKey->hidden_fileds;
-
-        } else {
-//==============Process when Api is public============
-            $this->hiddenFields = $checkApi->hidden_default;
-        }
+        parent::__construct($request);
+        $data          = $this->data;
+        $this->limit   = empty($data['limit']) ? 10 : $data['limit'];
+        $this->start   = empty($data['start']) ? 0 : (int) $data['start'];
+        $this->orderBy = empty($data['orderBy']) ? [] : explode(',', $data['orderBy']);
+        $this->sort    = $data['sort'] ?? 'ASC';
+        $this->model   = empty($data['model']) ? '' : $data['model'];
 
     }
 
