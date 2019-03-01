476d9b50ccca811d00c5fa0bb07a5b0232fd20e0
Naruto <lanhktc@gmail.com>
2019-03-01 21:50:35 +0700

    update search product control

--- config/scart.php
+++ config/scart.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version'  => '2.1.2',
+    'version'  => '2.1.3-beta',
     'homepage' => 'https://s-cart.org',
     'name'     => 'S-Cart',
     'title'    => 'Free Open Source eCommerce for Business',
