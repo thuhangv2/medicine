8abdf41f15dffeb83676db44cd666f8571d4a379
Naruto <lanhktc@gmail.com>
2019-03-24 10:17:34 +0700

    no message

--- resources/views/templates/default/right.blade.php
+++ resources/views/templates/default/right.blade.php
@@ -2,10 +2,10 @@
   @isset ($layouts['right'])
       @foreach ( $layouts['right']  as $element)
         @if ($element->page == null ||  $element->page =='*' || (isset($layout_page) && $element->page == $layout_page) )
-          @if ($element->type =='html' || $element->type =='block')
+          @if ($element->type =='html')
             {!! $element->content !!}
           @elseif($element->type =='view')
-            @include($theme.'.'.$element->content)
+            @include($theme.'.blockView.'.$element->content)
           @elseif($element->type =='module')
             {!! (new $element->content)->render() !!}
           @endif
