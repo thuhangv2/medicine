641bdf6d949bcee0e2088cb8d73db88e32cd7161
Naruto <lanhktc@gmail.com>
2019-05-19 17:12:41 +0700

    no message

--- app/Models/Layout.php
+++ app/Models/Layout.php
@@ -8,7 +8,7 @@ class Layout extends Model
 {
     public $timestamps  = false;
     public $table       = 'layout';
-    protected $fillable = ['status', 'content', 'sort', 'type', 'page', 'position', 'name'];
+    protected $fillable = ['status', 'text', 'sort', 'type', 'page', 'position', 'name'];
     /**
      * Get value field page same array
      * @param  [type] $page [description]
