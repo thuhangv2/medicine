fd61c99e9d32b44636957339fa459ca0ba5e9a2f
Naruto <lanhktc@gmail.com>
2019-10-10 06:01:07 +0700

    edit search order admin

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -14,9 +14,9 @@
       </div>
 
       <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
+      <form action="{{ route('admin_order.index') }}" method="get" class="sidebar-form">
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
+        <input type="text" name="keyword" class="form-control" placeholder="{{trans('order.search')}}">
           <span class="input-group-btn">
                 <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
